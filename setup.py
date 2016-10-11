e90120273d09d7a8c05418f433feae0e1fd8a974
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2016-10-11 23:36:35 +0200

    extend and simplify setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 
-from distutils.core import setup, Extension
+from setuptools import setup, find_packages
+from distutils.extension import Extension
 
 setup(
         name='SyncRNG',
         author='Gertjan van den Burg',
-        version='1.0',
+        version='1.1.0',
         description='A synchronized Tausworthe RNG for Python and R',
+        url='https://github.com/GjjvdBurg/SyncRNG',
         license='GPL v2',
-        package_dir={'': 'Python'},
-        packages=[''],
+        packages=find_packages(),
         ext_modules=[
             Extension(
                 "syncrng",
@@ -16,4 +17,13 @@ setup(
                 sources=["src/syncrng.c"]
                 )
             ],
+        keywords='RNG R Python',
+        classifiers=[
+            'Intended Audience :: Developers',
+            'Indended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Topic :: Scientific/Engineering :: Mathematics'
+            ]
         )
