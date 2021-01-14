661b3ec1a264e6d978d826800c369e1a999ee9a7
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2021-01-14 23:40:42 +0000

    Remove python 2.7 trove

--- python/setup.py
+++ python/setup.py
@@ -96,7 +96,6 @@ setup(
             'Intended Audience :: Developers',
             'Intended Audience :: Science/Research',
             'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-            'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
             'Topic :: Scientific/Engineering :: Mathematics'
     ],
