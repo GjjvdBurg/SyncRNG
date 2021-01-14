057f6a2f1a5df617e10e8162b48ae65cfce7429a
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2021-01-14 16:56:10 +0000

    Minor code quality fixes

--- SyncRNG/__init__.py
+++ SyncRNG/__init__.py
@@ -5,20 +5,20 @@ used to seed and pull numbers from the RNG.
 """
 
 from copy import deepcopy
-from warnings import warn as _warn
+from warnings import warn
 
 from _syncrng import seed as _seed
 from _syncrng import rand as _rand
 
-class SyncRNG(object):
 
+class SyncRNG(object):
     def __init__(self, seed=0):
         self.BPF = 32
         self.seed = seed
-        self.state = syncrng.seed(seed)
+        self.state = _seed(self.seed)
 
     def randi(self):
-        tmp = syncrng.rand(self.state)
+        tmp = _rand(self.state)
         self.state = tmp[:-1]
         return(tmp[-1])
 
@@ -26,22 +26,24 @@ class SyncRNG(object):
         return self.randi() * 2.3283064365387e-10
 
     def randbelow(self, n):
-        maxsize = 1<<self.BPF
+        maxsize = 1 << self.BPF
         if n >= maxsize:
-            _warn("Underlying random generator does not supply \n"
-                    "enough bits to choose from a population range this "
-                    "large.\n")
+            warn(
+                "Underlying random generator does not supply \n"
+                "enough bits to choose from a population range this "
+                "large.\n"
+            )
             return int(self.rand() * n)
         rem = maxsize % n
         limit = (maxsize - rem) / maxsize
         r = self.rand()
         while r >= limit:
             r = self.rand()
-        return int(r*maxsize) % n
+        return int(r * maxsize) % n
 
     def shuffle(self, x):
         y = deepcopy(x)
         for i in reversed(range(1, len(y))):
-            j = self.randbelow(i+1)
+            j = self.randbelow(i + 1)
             y[i], y[j] = y[j], y[i]
         return y
