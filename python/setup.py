093231544ee98819def970711634942d5984b245
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2021-01-15 16:29:07 +0000

    Remove old code from setup.py

--- python/setup.py
+++ python/setup.py
@@ -51,16 +51,10 @@ here = os.path.abspath(os.path.dirname(__file__))
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 
-# see if it hard fails on CI
+# We don't use try-except here because we want builds to fail if they can't 
+# find the README.
 with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = "\n" + f.read()
-
-# DEBUG OLD
-# try:
-#     with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-#         long_description = "\n" + f.read()
-# except FileNotFoundError:
-#     long_description = DESCRIPTION
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
