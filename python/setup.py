35c932bfb60350538757c4b212fb4554d4ce5cc4
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2021-01-15 13:55:53 +0000

    check if readme can be found on CI

--- python/setup.py
+++ python/setup.py
@@ -50,11 +50,17 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-        long_description = "\n" + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
+
+# see if it hard fails on CI
+with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
+# DEBUG OLD
+# try:
+#     with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+#         long_description = "\n" + f.read()
+# except FileNotFoundError:
+#     long_description = DESCRIPTION
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
